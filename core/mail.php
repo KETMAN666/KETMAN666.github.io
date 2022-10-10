23c03c664d81c9ccf84e38f5d83e523d44cbdcdf
KETMAN666 <nshsnjmnsm@gmail.com>
2022-10-10 22:21:11 +0300

    Update mail.php

--- core/mail.php
+++ core/mail.php
@@ -6,24 +6,24 @@ $json = json_decode($json, true);
 //письмо
 $message = '';
 $message .= '<h1>Заказ в магазине</h1>';
-$message .= '<p>Телефон: '.$_POST['ephone'].'</p>';
-$message .= '<p>Почта: '.$_POST['email'].'</p>';
-$message .= '<p>Клиент: '.$_POST['ename'].'</p>';
+$message .='<p>Телефон: '.$_POST['ephone'].'</p>';
+$message .='<p>Почта: '.$_POST['email'].'</p>';
+$message .='<p>Клиент: '.$_POST['ename'].'</p>';
 
 $cart = $_POST['cart'];
 $sum = 0;
 foreach ($cart as $id=>$count) {
-    $message .=$json[$id] ['name']. ' --- ';
-    $message .=$count. ' --- ';
-    $message .=$count*$json[$id] ['cost'];
+    $message .=$json[$id]['name'].' --- ';
+    $message .=$count.' --- ';
+    $message .=$count*$json[$id]['cost'];
     $message .='<br>';
-    $sum = $sum + $count*$json[$id] ['cost'];
+    $sum = $sum +$count*$json[$id]['cost'];
 }
 $message .='Всего: '.$sum;
 
 //print_r($message);
 
-$to = 'nshsnjmnsm@gmail.com'.',';
+$to = 'nshsnjmnsm@gmail.com'.','; //не забудь поменять!
 $to .=$_POST['email'];
 $spectext = '<!DOCTYPE HTML><html><head><title>Заказ</title></head><body>';
 $headers  = 'MIME-Version: 1.0' . "\r\n";
@@ -32,3 +32,4 @@ $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 $m = mail($to, 'Заказ в магазине', $spectext.$message.'</body></html>', $headers);
 
 if ($m) {echo 1;} else {echo 0;}
+
